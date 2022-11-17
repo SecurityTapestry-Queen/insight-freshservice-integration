6d4254801440dc5bd746ef6d1b6874c7ee552702
Natalya Higdon @ Security Tapestry <nhigdon@securitytapestry.com>
2022-11-17 12:42:49 -0600

    comment post test done

--- insightIfNewPostToFS.py
+++ insightIfNewPostToFS.py
@@ -2,6 +2,14 @@ import requests
 import json
 from datetime import datetime
 
+global lasttimedata
+global investigations
+global item
+global commentdata
+global ticketID
+
+API_KEY = "8ea43599-e617-4c6a-a106-5d9984df1332"
+
 def whenWasTheLastTime():
     lasttime = open("lasttime.txt", "r")
     global lasttimedata
@@ -12,7 +20,7 @@ def whenWasTheLastTime():
 def getInsightInvestigations():
     url = 'https://us2.api.insight.rapid7.com/idr/v2/investigations'
     headers = {
-    "X-Api-Key": "8ea43599-e617-4c6a-a106-5d9984df1332",
+    "X-Api-Key": API_KEY,
     "Accept-version": "investigations-preview"
     }
     params = {
@@ -38,6 +46,7 @@ def checkForNew():
             global item
             item = i
             postTicketToFS()
+            getInvestigationComments(item["rrn"])
 
 def updateLastTime():
     lasttime = open("lasttime.txt", "w")
@@ -63,8 +72,42 @@ def postTicketToFS():
     "status":2,
     "priority":idr_priority
     }
-    requests.post(webhook_url, auth=('p4CWhwgKyWmyrJrKWJ', 'X'), data=json.dumps(data), headers= {'Content-Type': 'application/json'})
+    global ticketID
+    r = requests.post(webhook_url, auth=('p4CWhwgKyWmyrJrKWJ', 'X'), data=json.dumps(data), headers= {'Content-Type': 'application/json'})
+    ticketID = r.json()["ticket"]["id"]
+    print(ticketID)
+    # print(ticketResponse["ticket"]["id"])
     # print(data)
+
+def getInvestigationComments(id):
+    url = 'https://us2.api.insight.rapid7.com/idr/v1/comments'
+    headers = {
+    "X-Api-Key": API_KEY,
+    "Accept-version": "comments-preview"
+    }
+    params = {
+    "multi-customer": True,
+    "target": id
+    }
+
+    r = requests.get(url, headers=headers, params=params)
+    global commentdata
+    comments = r.json()
+    commentdata = comments["data"]
+    for c in commentdata:
+        created = datetime.strptime(c["created_time"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        checktime = datetime.strptime(lasttimedata, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+        if checktime > created:
+            continue
+        elif c["body"] is None:
+            continue
+        else:
+            print(
+                c["created_time"] + "\n"
+                + c["creator"]["name"] + "\n"
+                + c["body"]
+                )
 
 # Execution Block
 print("whenWasTheLastTime")
@@ -73,4 +116,4 @@ print("getInsightInvestigations")
 getInsightInvestigations()
 print("checkForNew")
 checkForNew()
-updateLastTime()
+# updateLastTime()
